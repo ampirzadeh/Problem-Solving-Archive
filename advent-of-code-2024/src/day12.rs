4bf8e49dd567237ccf17a44d5e8676b8a55a4b21
AmirMohammad Pirzadeh <am.pirzadeh@gmail.com>
2024-12-13 20:40:42 +0000

    Advent of code day 12 and 13

---
+++ advent-of-code-2024/src/day12.rs
@@ -0,0 +1,68 @@
+use std::{collections::HashMap, hash::Hash, os::unix::fs::PermissionsExt};
+
+use advent_of_code_2024::{Point, Solution};
+
+pub struct Day12 {
+    pub input: String,
+}
+
+impl Day12 {
+    fn get_matrix(&self) -> Vec<Vec<char>> {
+        self.input
+            .split("\n")
+            .map(|x| x.chars().collect())
+            .collect()
+    }
+
+    fn get_point(&self, point: Point) -> Option<char> {
+        let matrix = self.get_matrix();
+        matrix
+            .get(point.x as usize)
+            .and_then(|f| f.get(point.y as usize))
+            .copied()
+    }
+}
+
+impl Solution for Day12 {
+    fn part1(&self) -> i128 {
+        let mut areas: HashMap<char, i128> = HashMap::new();
+        let mut perimeters: HashMap<char, i128> = HashMap::new();
+
+        for (row, roww) in self.get_matrix().iter().enumerate() {
+            for (col, coll) in roww.iter().enumerate() {
+                areas.entry(*coll).and_modify(|f| *f += 1).or_insert(1);
+
+                let top = self.get_point(Point::new(row as i128 - 1, col as i128));
+                if top.is_none() || top.is_some_and(|f| f != *coll) {
+                    perimeters.entry(*coll).and_modify(|f| *f += 1).or_insert(1);
+                }
+                let bottom = self.get_point(Point::new(row as i128 + 1, col as i128));
+                if bottom.is_none() || bottom.is_some_and(|f| f != *coll) {
+                    perimeters.entry(*coll).and_modify(|f| *f += 1).or_insert(1);
+                }
+                let left = self.get_point(Point::new(row as i128, col as i128 - 1));
+                if left.is_none() || left.is_some_and(|f| f != *coll) {
+                    perimeters.entry(*coll).and_modify(|f| *f += 1).or_insert(1);
+                }
+                let right = self.get_point(Point::new(row as i128, col as i128 + 1));
+                if right.is_none() || right.is_some_and(|f| f != *coll) {
+                    perimeters.entry(*coll).and_modify(|f| *f += 1).or_insert(1);
+                }
+            }
+        }
+
+        println!("areas: {:?}\nperimeters: {:?}", areas, perimeters);
+
+        let mut cost = 0;
+
+        for (key, value) in &areas {
+            cost += perimeters.get(key).unwrap() * value;
+            println!("cost of {key} is {}", perimeters.get(key).unwrap() * value);
+        }
+        cost
+    }
+
+    fn part2(&self) -> i128 {
+        0
+    }
+}
