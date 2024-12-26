469d2038811b659aeaca783f33a0c4f855d603f6
ampirzadeh <am.pirzadeh@gmail.com>
2024-12-26 18:14:40 +0000

    Advent of code 2024 day 18, 19!

---
+++ advent-of-code-2024/src/day17.rs
@@ -0,0 +1,15 @@
+use advent_of_code_2024::Solution;
+
+pub struct Day17 {
+    pub input: String,
+}
+
+
+impl Solution for Day17 {
+    fn part1(&self) -> i128 {
+        todo!();
+    }
+    fn part2(&self) -> i128 {
+        todo!();
+    }
+}
