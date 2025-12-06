5817fb5570b5d0723f459812d154f406de58de1a
ampirzadeh <am.pirzadeh@gmail.com>
2025-12-06 14:34:24 +0000

    Advent of Code 2025 Day 6

--- advent-of-code-2025/src/main.rs
+++ advent-of-code-2025/src/main.rs
@@ -4,6 +4,7 @@ use day2::Day2;
 use day3::Day3;
 use day4::Day4;
 use day5::Day5;
+use day6::Day6;
 use std::{
     env,
     fs::{self},
@@ -15,6 +16,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn file(day_number: usize) -> String {
     let path = PathBuf::from(format!("src/data/day{}.txt", day_number));
@@ -25,12 +27,13 @@ fn file(day_number: usize) -> String {
 }
 
 fn main() {
-    let solutions: [Box<dyn Solution>; 5] = [
+    let solutions: [Box<dyn Solution>; 6] = [
         Box::new(Day1 { input: file(1) }),
         Box::new(Day2 { input: file(2) }),
         Box::new(Day3 { input: file(3) }),
         Box::new(Day4 { input: file(4) }),
         Box::new(Day5 { input: file(5) }),
+        Box::new(Day6 { input: file(6) }),
     ];
 
     match env::args()
