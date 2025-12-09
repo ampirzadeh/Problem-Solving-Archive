209a5d32f92d4eae4f379e6774fc7da96d5189fc
ampirzadeh <am.pirzadeh@gmail.com>
2025-12-09 14:42:53 +0000

    Advent of Code 2025 Day 9

--- advent-of-code-2025/src/lib.rs
+++ advent-of-code-2025/src/lib.rs
@@ -6,6 +6,62 @@ pub trait Solution {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct Vector2d {
+    pub x: i64,
+    pub y: i64,
+}
+
+impl Add for Vector2d {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Vector2d {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Vector2d {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Vector2d {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl Mul<i64> for Vector2d {
+    type Output = Self;
+
+    fn mul(self, other: i64) -> Self::Output {
+        Vector2d {
+            x: self.x * other,
+            y: self.y * other,
+        }
+    }
+}
+
+impl Rem for Vector2d {
+    type Output = Self;
+
+    fn rem(self, other: Self) -> Self::Output {
+        Vector2d {
+            x: self.x % other.x,
+            y: self.y % other.y,
+        }
+    }
+}
+
+impl Vector2d {
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Vector3d {
     pub x: i64,
     pub y: i64,
