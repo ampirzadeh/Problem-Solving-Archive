7468571ff0332d12ee6aa6b562e12fd59ceb2521
ampirzadeh <am.pirzadeh@gmail.com>
2025-12-09 13:39:52 +0000

    Advent of Code 2025 Day 8 Part 2 INCOMPLETE

--- advent-of-code-2025/src/day8.rs
+++ advent-of-code-2025/src/day8.rs
@@ -9,14 +9,13 @@ pub struct Day8 {
     pub input: String,
 }
 
-impl Day8 {}
+type Circuits = Vec<Vec<usize>>;
+type DistanceMap = HashMap<(usize, usize), i64>;
 
-impl Solution for Day8 {
-    fn part1(&self) -> String {
-        const PAIRS_TO_TAKE_COUNT: usize = 1000;
-
+impl Day8 {
+    fn get_points(input: &String) -> Vec<Vector3d> {
         let mut points: Vec<Vector3d> = vec![];
-        for line in self.input.lines() {
+        for line in input.lines() {
             let s = line
                 .split(",")
                 .map(|f| f.parse::<i64>().unwrap())
@@ -28,12 +27,15 @@ impl Solution for Day8 {
                 z: s[2],
             })
         }
+        points
+    }
 
+    fn get_distance_map(points: &Vec<Vector3d>) -> DistanceMap {
         // This is basically a matrix of distances, except the matrix would have been symmetric and
         // so would have wasted memory
         // key: indices of source and destination in points[]
         // value: distance between the points
-        let mut distance_map: HashMap<(usize, usize), i64> = HashMap::new();
+        let mut distance_map: DistanceMap = HashMap::new();
         // build the distance map
         for i in 0..points.len() {
             for j in i + 1..points.len() {
@@ -45,13 +47,29 @@ impl Solution for Day8 {
             }
         }
 
-        // an iterator of the first 1000 pairs after they're sorted by distance
-        let wanted_pairs = distance_map
-            .iter()
-            .sorted_by_key(|x| x.1)
-            .take(PAIRS_TO_TAKE_COUNT);
+        distance_map
+    }
+    fn get_circuits(
+        distance_map: &DistanceMap,
+        pairs_to_take_count: usize,
+        points: &Vec<Vector3d>,
+    ) -> Circuits {
+        // wanted_pairs is an iterator of the n smallest pairs after they're sorted by distance
+        //
+        // My initial Solution:
+        // let wanted_pairs = distance_map
+        //     .iter()
+        //     .sorted_by_key(|x| x.1)
+        //     .take(pairs_to_take_count);
+        //
+        // Much better performance as it doesn't sort the whole distance_map
+        let mut items: Vec<_> = distance_map.iter().collect();
+        if items.len() > pairs_to_take_count {
+            items.select_nth_unstable_by_key(pairs_to_take_count, |x| x.1);
+        }
+        let wanted_pairs = items[..pairs_to_take_count].iter();
 
-        let mut circuits: Vec<Vec<usize>> = vec![];
+        let mut circuits: Circuits = vec![];
 
         // add p1 and p2 to circuits appropriately
         for ((p1, p2), _d) in wanted_pairs {
@@ -59,6 +77,10 @@ impl Solution for Day8 {
             let c1_index = circuits.iter().position(|c| c.contains(p1));
             let c2_index = circuits.iter().position(|c| c.contains(p2));
 
+            // println!(
+            //     "adding {p1} and {p2} which are points in circuits {:?} and {:?}",
+            //     points[*p1], points[*p2]
+            // );
             // PATTERN MATCHING IS GREAT
             match (c1_index, c2_index) {
                 // neither is in any circuit so create a new circuit
@@ -91,6 +113,18 @@ impl Solution for Day8 {
         }
 
         circuits
+    }
+}
+
+impl Solution for Day8 {
+    fn part1(&self) -> String {
+        const PAIRS_TO_TAKE_COUNT: usize = 10;
+
+        let points = Self::get_points(&self.input);
+        let distance_map = Self::get_distance_map(&points);
+        let circuits = Self::get_circuits(&distance_map, PAIRS_TO_TAKE_COUNT, &points);
+
+        circuits
             .iter()
             .map(|f| f.len()) // get the length of each circuit
             .sorted() // sort them ascending
@@ -101,6 +135,34 @@ impl Solution for Day8 {
     }
 
     fn part2(&self) -> String {
+        let mut pair_count = 3580;
+
+        let points = Self::get_points(&self.input);
+        let distance_map = Self::get_distance_map(&points);
+
+        let mut circuits;
+        loop {
+            circuits = Self::get_circuits(&distance_map, pair_count, &points);
+
+            let a = distance_map
+                .iter()
+                .sorted_by_key(|x| x.1)
+                .nth(pair_count - 1)
+                .unwrap()
+                .to_owned();
+            println!(
+                "ADDING {} AND {} WHICH ARE POINTS IN CIRCUITS {:?} AND {:?} -> {}",
+                a.0 .0, a.0 .1, points[a.0 .0], points[a.0 .1], points[a.0 .0].x * points[a.0 .1].x
+            );
+
+            pair_count += 1;
+            println!("{} {}", pair_count, circuits.len());
+
+            if circuits.len() == 1 {
+                break;
+            }
+        }
+
         0.to_string()
     }
 }
